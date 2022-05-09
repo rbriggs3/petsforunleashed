29905232b763bcf05ce1ee30e9fad0de17661eab
rbriggs3 <12.briggs@gmail.com>
2022-05-08 18:01:05 -0700

    react-navigation

--- frontend/App.js
+++ frontend/App.js
@@ -2,28 +2,25 @@ import { ResponseType } from "expo-auth-session";
 import * as Facebook from "expo-auth-session/providers/facebook";
 import * as WebBrowser from "expo-web-browser";
 import * as React from "react";
-import { Button, Image, StyleSheet, Text, View } from "react-native";
+import { Button, Image, StyleSheet, Text, View, Linking } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 WebBrowser.maybeCompleteAuthSession();
 
 const FB_APP_ID = process.env.REACT_APP_FB_ID;
 
-export default function App() {
+function FBAuth({ navigation }) {
   const [user, setUser] = React.useState(null);
   // Request
   const [request, response, promptAsync] = Facebook.useAuthRequest({
     clientId: FB_APP_ID,
-    // NOTICE: Please do not actually request the token on the client (see:
-    // response_type=token in the authUrl), it is not secure. Request a code
-    // instead, and use this flow:
-    // https://developers.facebook.com/docs/facebook-login/manually-build-a-login-flow/#confirm
-    // The code here is simplified for the sake of demonstration. If you are
-    // just prototyping then you don't need to concern yourself with this and
-    // can copy this example, but be aware that this is not safe in production.
     responseType: ResponseType.Token,
-  });
+  })
 
-  if (request) {console.log('REACT_APP_FB_ID', process.env.REACT_APP_FB_ID)}
+  if (request) {console.log('REACT_APP_FB_ID', FB_APP_ID)}
 
   React.useEffect(() => {
     if (response && response.type === "success" && response.authentication) {
@@ -44,15 +41,16 @@ export default function App() {
       return;
     }
   };
-
+  
   return (
-    <View style={styles.container}>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       {user ? (
-        <Profile user={user} />
+        navigation.navigate('Home')
       ) : (
         <Button
           disabled={!request}
-          title="Open FB Auth"
+          title="Continue with Facebook"
+          color="#0870ea"
           onPress={handlePressAsync}
         />
       )}
@@ -60,15 +58,97 @@ export default function App() {
   );
 }
 
-function Profile({ user }) {
+function HomeScreen() {
   return (
-    <View style={styles.profile}>
-      <Image source={{ uri: user.picture.data.url }} style={styles.image} />
-      <Text style={styles.name}>{user.name}</Text>
-      <Text>ID: {user.id}</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={styles.space} />
+      <JaxAndZed/>
+      <Text>Home Screen</Text>
     </View>
   );
 }
+
+function ContactScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button onPress={() => Linking.openURL('https://www.instagram.com/petsforunleashed/') }
+        title="Check out our Instagram"
+        color="#4f1416"
+        />
+      <View style={styles.space} />
+      <Button onPress={() => Linking.openURL('mailto:tmcowley@live.com?subject=Conctact: Pets For Unleashed') }
+      title="Send us an Email"
+      color="#4f1416"
+      />
+    </View>
+  );
+}
+
+function LogoTitle() {
+  return (
+    <Image
+      style={{ width: 50, height: 50 }}
+      source={require('./img/paw.png')}
+    />
+  );
+}
+
+function JaxAndZed() {
+  return (
+    <Image
+      style={{ width: 100, height: 100 }}
+      source={require('./img/boys.jpg')}
+    />
+  );
+}
+
+function Profile({ user }) {
+  return (
+    <Image 
+      source={{ uri: user.picture.data.url }}
+      style={styles.image}
+    />
+  );
+}
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={FBAuth}
+          options={{
+            headerShown: () => {false},
+          }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={({ navigation }) => ({
+            headerTitle: props => <LogoTitle {...props} />,
+            headerRight: () => (
+              <Button
+                onPress={() => navigation.navigate('Contact')}
+                title="Contact"
+                color="#4f1416"
+              />
+            )
+          })}
+        />
+        <Stack.Screen
+          name="Contact"
+          component={ContactScreen}
+          options={({ navigation }) => ({
+            headerTitle: props => <LogoTitle {...props} />
+          })}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
@@ -83,8 +163,12 @@ const styles = StyleSheet.create({
     fontSize: 20,
   },
   image: {
-    width: 100,
-    height: 100,
+    width: 55,
+    height: 55,
     borderRadius: 50,
+  },
+  space: {
+    width: 20, // or whatever size you need
+    height: 20,
   },
 });
